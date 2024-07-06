8664e2b7564e49783e3a3a908fcb81f45c694578
Chaosjan44 <j.m.schniebs@gmail.com>
2024-07-06 15:59:53 +0200

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
         <!-- Desktop -->
         <div class="container-fluid py-3">
             <h2 class="text-center text-kolping-orange">Petition</h2>
-            <h1 class="display-4 text-center">Forderung and die Regierung den Hochwasseropfern<br>in Rudersberg & Umgebung zu helfen</h1>
+            <h1 class="display-4 text-center">Forderung an die Regierung den Hochwasseropfern<br>in Rudersberg & Umgebung zu helfen</h1>
             <div class="row py-3">
                 <div class="col-2"></div>
                 <div class="col-3"><img src="imgs/petition-qrcode.png" alt="QR-Code zu der Petition" class="img-fluid"><span><a href="https://chng.it/BLv9tWxcYH" class="text-kolping-orange text-size-x-large text-center">https://chng.it/BLv9tWxcYH</a></span></div>
@@ -33,7 +33,7 @@
         <!-- Mobile -->
         <div class="container-fluid pt-3">
             <h2 class="text-center text-kolping-orange">Petition</h2>
-            <h1 class="display-4 text-center">Forderung and die Regierung den Hochwasseropfern<br>in Rudersberg & Umgebung zu helfen</h1>
+            <h1 class="display-4 text-center">Forderung an die Regierung den Hochwasseropfern<br>in Rudersberg & Umgebung zu helfen</h1>
             <div class="row py-3">
                 <div class="col-1"></div>
                 <div class="col-10"><img src="imgs/petition-qrcode.png" alt="QR-Code zu der Petition" class="img-fluid"><span><a href="https://chng.it/BLv9tWxcYH" class="text-kolping-orange text-size-x-large text-center">https://chng.it/BLv9tWxcYH</a></span></div>
